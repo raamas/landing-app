d346e28cef24c79fc627eb8a7f5e6a30947be642
Estudiante <Raamas>
2025-05-23 12:12:00 -0500

    add adapter

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,23 +1,23 @@
-export const Form ({handler}){
-  return(
+export const Form = ({ handler }) =>{
+  return (
     <form
-  class="mt-4 flex flex-col sm:flex-row items-center gap-4 w-full"
-  onSubmit={(e)=>handler(e)}
-  method="POST"
->
-  <input
-    type="email"
-    name="email"
-    placeholder="Enter your email"
-    class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-    required
-  />
-  <button
-    type="submit"
-    class="w-full text-nowrap sm:w-auto px-6 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition"
-  >
-    Notify Me
-  </button>
-</form>
-    )
+      class="mt-4 flex flex-col sm:flex-row items-center gap-4 w-full"
+      onSubmit={(e) => handler(e)}
+      method="POST"
+    >
+      <input
+        type="email"
+        name="email"
+        placeholder="Enter your email"
+        class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+        required
+      />
+      <button
+        type="submit"
+        class="w-full text-nowrap sm:w-auto px-6 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition"
+      >
+        Notify Me
+      </button>
+    </form>
+  )
 }
