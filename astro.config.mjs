b1d0436c1afd3869167e639e527cb59ec6edcc7d
Estudiante <Raamas>
2025-05-23 12:10:31 -0500

    add adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   output : "server",
@@ -9,5 +11,7 @@ export default defineConfig({
     plugins: [
       tailwindcss()
     ]
-  }
+  },
+
+  adapter: vercel()
 });
